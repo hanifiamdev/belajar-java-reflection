c76da8359528b8cfe8ad0a53b806da2e7f809f82
hanifiamdev <hanifamrullahdev@gmail.com>
2021-12-23 17:10:58 +0700

    Method

---
+++ src/test/java/com/hanifiamdev/reflection/MethodTest.java
@@ -0,0 +1,39 @@
+package com.hanifiamdev.reflection;
+
+import com.hanifiamdev.reflection.data.Person;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+
+public class MethodTest {
+
+    @Test
+    void testGetMethods() {
+
+        Class<Person> personClass = Person.class;
+
+        Method[] methods = personClass.getDeclaredMethods();
+        for (Method method :
+                methods) {
+            System.out.println(method.getName());
+            System.out.println(method.getReturnType());
+            System.out.println(Arrays.toString(method.getParameterTypes()));
+            System.out.println(Arrays.toString(method.getExceptionTypes()));
+            System.out.println("==========");
+        }
+    }
+
+    @Test
+    void testGetMethodsValue() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+
+        Class<Person> personClass = Person.class;
+        Method getFirstName = personClass.getDeclaredMethod("getFirstName");
+
+        Person person = new Person("Hanif", "Amrullah");
+        String firstName = (String) getFirstName.invoke(person);
+        System.out.println(firstName);
+
+    }
+}
